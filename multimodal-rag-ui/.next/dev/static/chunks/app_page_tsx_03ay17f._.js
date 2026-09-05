1b4a9678ad6c658eee7116a5942d7ddbc463e882
Anas Sabir <anassabir1072@gmail.com>
2026-09-05 17:27:57 +0500

    Delete multimodal-rag-ui/.next/dev directory

--- multimodal-rag-ui/.next/dev/static/chunks/app_page_tsx_03ay17f._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0i66ntv._.js"],
-    source: "dynamic"
-});
